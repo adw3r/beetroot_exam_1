5e96a0ce2f70e839acc5960740ade05f357a243b
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-07-18 00:17:01 +0300

    video with animation

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ const lazyLoadInstance = new LazyLoad({
     elements_selector: ".lazy"
 });
 lazyLoadInstance.update()
+
+const displayVideo = (btn) => {
+    const frame= document.querySelector('iframe')
+
+    frame.style.visibility = "visible";
+    frame.style.opacity = "1";
+    btn.style.display = "none";
+}
+document.displayVideo = displayVideo
