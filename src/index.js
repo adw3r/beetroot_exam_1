33d80f9d9c65d0942e303a4bcc29e8e642c8e7fa
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-07-18 17:38:11 +0300

    animated hover header

--- src/index.js
+++ src/index.js
@@ -13,3 +13,18 @@ const displayVideo = (btn) => {
     btn.style.display = "none";
 }
 document.displayVideo = displayVideo
+
+const toggleMenu = () => {
+    document.querySelectorAll('.js-hamburger').forEach(
+        function (el) {
+            el.classList.toggle('is-active');
+            let mobile_menu = document.querySelector('.mobile-menu');
+            if (mobile_menu.classList.contains('open')) {
+                mobile_menu.classList.remove('open');
+            } else {
+                mobile_menu.classList.add('open');
+            }
+        }
+    )
+}
+document.toggleMenu = toggleMenu;
